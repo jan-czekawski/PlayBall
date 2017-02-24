07ba160cb043cba2187017846fc4a1d48b14d81a
michal <bihone@wp.pl>
2017-02-24 19:52:49 +0100

    add styling of modals and fix autofocus problem

--- app/assets/javascripts/footer_1.js
+++ app/assets/javascripts/footer_1.js
@@ -5,4 +5,6 @@ $("#loginButton").on("click", function(){
 $("#signupButton").on("click", function(){
 });
 
-
+$('.modal').on('shown.bs.modal', function() {
+  $(this).find('[autofocus]').focus();
+});
