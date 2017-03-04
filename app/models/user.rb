9e66d0821880703d33875e96960a63b43553666d
michal <bihone@wp.pl>
2017-03-04 10:30:15 +0100

    Add password confirmation and validation of email uniqueness on the database level

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,16 @@
 class User < ApplicationRecord
+
   has_many :courts, dependent: :destroy
   has_many :comments, dependent: :destroy
   before_save { self.email = email.downcase }
-  validates :username, presence: true, 
-                       uniqueness: { case_sensitive: false}, 
-                       length: { minimum: 3, maximum: 25}
+  validates :username, presence: true,
+                       length: { maximum: 50}
   VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, 
-                    length: { maximum: 100 }, 
+                    length: { maximum: 255 }, 
                     uniqueness: { case_sensitive: false },
                     format: { with: VALID_EMAIL_REGEX }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true, confirmation: true
+  validates_confirmation_of :password
 end
