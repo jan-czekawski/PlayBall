ffa995a55aace380abb1dac0cec4d49d11e24c95
michal <bihone@wp.pl>
2017-03-05 05:33:50 +0100

    Add different views for courts page, whether user is logged in or not

--- app/assets/javascripts/autocomplete.js
+++
@@ -1,8 +0,0 @@
-  function initializeMap(){
-        var input = document.getElementById('court_location');
-        var autocomplete = new google.maps.places.Autocomplete(input);
-
-        google.maps.event.addListener(autocomplete, 'place_changed',function(){
-        });
-    };
-google.maps.event.addDomListener(window, 'pageshow', initializeMap);
--- app/assets/javascripts/custom_scripts.js
+++ app/assets/javascripts/custom_scripts.js
@@ -1,3 +1,51 @@
+function displayMap(){
+  function initMapOn(latSelected, lngSelected) {
+    var myLatLng = {lat: latSelected, lng: lngSelected};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 12,
+      center: myLatLng
+    });
+
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      title: 'Hello World!'
+    });
+  }
+
+  window.onload = function(){
+    var long = parseFloat($("#inputLongitude").val());
+    var lat = parseFloat($("#inputLatitude").val());
+    setTimeout(function(){
+      initMapOn(lat, long);
+    }, 50)
+  }
+}
+
+function displayAutoComplete(){
+  function initializeMap(){
+      var input = document.getElementById('court_location');
+      var autocomplete = new google.maps.places.Autocomplete(input);
+
+      google.maps.event.addListener(autocomplete, 'place_changed',function(){
+      });
+  };
+  google.maps.event.addDomListener(window, 'pageshow', initializeMap);
+}
+
+function changeHeight() {
+  $('#mapDiv').height($('#showImg').height());
+}
+
+$(function() {
+    $("textarea").flexible();
+});
+
+$(function() {
+    $('.thumbnail').matchHeight();
+});
+
 $('.modal').on('shown.bs.modal', function() {
   $(this).find('[autofocus]').focus();
 });
@@ -10,10 +58,6 @@ $('#court_picture').on('change', function(){
   }
 });
 
-
-$(function() {
-    $('.thumbnail').matchHeight();
-});
 
 $("#addCommentButton").on('click', function(event){
   event.preventDefault();
@@ -50,13 +94,8 @@ $(".close").on('click', function(){
   $(".addComment").hide();
 })
 
-$(function() {
-    $("textarea").flexible();
-});
 
-function changeHeight() {
-  $('#mapDiv').height($('#showImg').height());
-}
+
 
 $(document).ready(function(){
   var styles = {
@@ -66,7 +105,7 @@ $(document).ready(function(){
     marginLeft: "0",
     marginRight: "0"
   };
-  $( ".showJumbo" ).prev().prev().css(styles);
+  $( ".showJumbo" ).prev().css(styles);
   changeHeight();
 })
 
@@ -82,3 +121,19 @@ $(window).resize(function(){
 //     .done(function(msg){
 //     })
 //   }
+
+if ($.contains(document, $('#landingJumbotron')[0])) {
+  $('#mainNav').hide();
+  $('body').css("padding-top", 0);
+}
+
+if ($.contains(document, $('#mapDiv')[0])){
+  displayMap();
+}
+
+if ($.contains(document, $('#court_location')[0])){
+  displayAutoComplete();
+}
+
+
+
