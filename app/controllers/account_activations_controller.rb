37ecd8c1450b9f11df89244d2cfac46ded0c8352
michal <bihone@wp.pl>
2017-04-28 11:35:17 +0200

    Update account activation and application controller - ruby guidelines

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,13 +1,15 @@
 class AccountActivationsController < ApplicationController
   def edit
     user = User.find_by(email: params[:email])
-    if user && user.authenticated?("activation", params[:id]) && !user.activated?
-      user.update_columns(:activated => true, :activated_at => Time.now)
-      flash[:success] = "Your account's just been activated! You can log in now."
-      redirect_to courts_path
+    if user && user.authenticated?("activation", params[:id]) &&
+       !user.activated?
+      user.update_columns(activated: true, activated_at: Time.now)
+      flash[:success] = "Your account's just been activated!"\
+                        " You can log in now."
     else
-      flash[:danger] = "Your activation was not successfull. Link was not valid."
-      redirect_to courts_path
+      flash[:danger] = "Your activation was not successfull."\
+                       " Link was not valid."
     end
+    redirect_to courts_path
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,44 +1,35 @@
 class ApplicationController < ActionController::Base
-  
   protect_from_forgery with: :exception
   include SessionsHelper
   WillPaginate.per_page = 15
 
-  
-
   def require_user
-    if !logged_in?
-      flash[:danger] = "You must be logged in to perform that action"
-      redirect_to login_path
-    end
+    return if logged_in?
+    flash[:danger] = "You must be logged in to perform that action"
+    redirect_to login_path
   end
 
   def require_same_user
-    if current_user != @user && !current_user.admin?
-      flash[:danger] = "You can only perform that action for your own account"
-      redirect_to @user
-    end
+    return if current_user == @user || current_user.admin?
+    flash[:danger] = "You can only perform that action for your own account"
+    redirect_to @user
   end
 
   def require_same_user_for_courts
-    if current_user.id != @court.user_id && !current_user.admin?
-      flash[:danger] = "You can only perform that action for your own courts"
-      redirect_to @court
-    end
+    return if current_user.id == @court.user_id || current_user.admin?
+    flash[:danger] = "You can only perform that action for your own courts"
+    redirect_to @court
   end
 
   def require_same_user_for_comments
-    if current_user.id != @comment.user_id && !current_user.admin?
-      flash[:danger] = "You can only perform that action for your own comments"
-      redirect_to court_path(params[:court_id])
-    end
+    return if current_user.id == @comment.user_id || current_user.admin?
+    flash[:danger] = "You can only perform that action for your own comments"
+    redirect_to court_path(params[:court_id])
   end
 
   def require_admin
-    if !current_user.admin?
-      flash[:danger] = "Only Admins can perform that action"
-      redirect_to users_path
-    end
+    return if current_user.admin?
+    flash[:danger] = "Only Admins can perform that action"
+    redirect_to users_path
   end
-
 end
