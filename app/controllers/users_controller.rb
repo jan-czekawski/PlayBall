025faa404a7f447687131fe981349a06e7a35057
michal <bihone@wp.pl>
2017-05-01 23:13:52 +0200

    Update users controller - ruby guidelines

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,22 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :require_user, only: [:index, :show, :edit, :update, :destroy]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :set_user, only: %i[show edit update destroy]
+  before_action :require_user, only: %i[index show edit update destroy]
+  before_action :require_same_user, only: %i[edit update]
   before_action :require_admin, only: [:destroy]
 
   def index
-    @users = User.paginate(:page => params[:page])
+    @users = User.paginate(page: params[:page])
   end
 
   def show
-    @courts = Court.paginate(:page => params[:page]).where(:user_id => @user.id)
+    @courts = Court.paginate(page: params[:page]).where(user_id: @user.id)
   end
 
   def new
     @user = User.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @user = User.new(user_params)
@@ -28,13 +27,13 @@ class UsersController < ApplicationController
       # flash[:success] = 'User was successfully created.'
       redirect_to courts_path
     else
-      render 'new'
+      render "new"
     end
   end
 
   def update
     if @user.update(user_params)
-      flash[:info] = 'User was successfully updated.'
+      flash[:info] = "User was successfully updated."
       redirect_to @user
     else
       render "edit"
@@ -42,24 +41,21 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    if current_user == @user
-      session[:user_id] = nil
-    end
+    session[:user_id] = nil if current_user == @user
     @user.destroy
-    flash[:danger] = 'User was successfully destroyed.'
+    flash[:danger] = "User was successfully destroyed."
     redirect_to users_path
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:username, :email, :password, :password_confirmation, :picture)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-
+  def user_params
+    params.require(:user).permit(:username, :email, :password,
+                                 :password_confirmation, :picture)
+  end
 end
