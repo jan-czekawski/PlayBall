de9d98356cf9b5de872b32eb459067ac8bb5e602
Michal <bihone88@gmail.com>
2018-03-15 09:16:48 +0000

    Refactor models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,11 @@ class Comment < ApplicationRecord
   belongs_to :court, foreign_key: :court_id
   belongs_to :user
   default_scope -> { order(updated_at: :desc) }
-  validates :user_id, presence: true
-  validates :court_id, presence: true
   validates :content, presence: true
+  
+  def display_errors
+    error = "There is an error. "
+    errors.full_messages.each { |msg| error << msg + ". " }
+    error
+  end
 end
