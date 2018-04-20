b9ddda3cb9076203a816fe1e1a4f5ac468282480
Michal <bihone88@gmail.com>
2018-04-20 20:09:36 +0000

    Add default value for picture uploader if picture wasn't updated.

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -19,12 +19,14 @@ class PictureUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
+  def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+    # ["/assets/ball.jpg", "/assets/basket.jpg", "/assets/court1.jpg"].sample
+    "/assets/basket.jpg"
+  end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
