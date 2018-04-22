3dac98670a46345f86448f43520ac52076b82d0e
Michal <bihone88@gmail.com>
2018-04-22 08:59:08 +0000

    Add comment to fix display problem of user index page.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  # TODO: index users template doesn't show the whole card -> it will after refresh, check js and css
   attr_accessor :remember_token, :activation_token, :reset_token
   include ApplicationHelper
   before_create :create_activation_digest
