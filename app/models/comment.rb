86378fc4ec56927dea1460796ff066b3727f08b4
Michal <bihone88@gmail.com>
2018-04-22 08:58:45 +0000

    Change default scope from updated_at to created_at to sort courts and comments by creation date.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :court, foreign_key: :court_id
   belongs_to :user
-  default_scope -> { order(updated_at: :desc) }
+  default_scope -> { order(created_at: :desc) }
   validates :content, presence: true
   
   def display_errors
